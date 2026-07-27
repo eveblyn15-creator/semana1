9190320c30330a701a4a357dd8bc6f9d90d9a47d
eveblyn15-creator <eveblyn15@gmail.com>
2026-07-26 19:33:33 -0600

    Create ejemplo4.php

---
+++ ejemplo4.php
@@ -0,0 +1,48 @@
+<?php 
+abstract class Vehiculo 
+{ 
+    protected string $marca;
+     public function __construct(string $marca) 
+    { 
+        $this->marca = $marca; 
+    } 
+  
+    public function arrancar(): string 
+    { 
+        return "{$this->marca}: el motor enciende."; 
+    } 
+} 
+  
+class Carro extends Vehiculo 
+{ 
+    public function abrirCajuela(): string 
+    { 
+        return "{$this->marca}: cajuela abierta."; 
+    } 
+} 
+  
+class Moto extends Vehiculo 
+{ 
+    public function hacerCaballito(): string 
+    { 
+        return "{$this->marca}: ¡wheelie!"; 
+    } 
+} 
+class Bicicleta extends Vehiculo 
+{ 
+    public function correrRapido(): string 
+    { 
+        return "{$this->marca}: ¡lleva canasta!"; 
+    } 
+} 
+  
+$carro = new Carro("Toyota Corolla"); 
+$moto = new Moto("Honda CB190R"); 
+$bicicleta = new Bicicleta("Corsario");
+  
+echo $carro->arrancar() . PHP_EOL; 
+echo $carro->abrirCajuela() . PHP_EOL; 
+echo $moto->arrancar() . PHP_EOL; 
+echo $moto->hacerCaballito() . PHP_EOL; 
+echo $bicicleta->arrancar() . PHP_EOL; 
+echo $bicicleta->correrRapido() . PHP_EOL; 
